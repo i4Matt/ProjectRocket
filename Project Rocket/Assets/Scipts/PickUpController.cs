ac35fb81912840a6c31fb505220df0d29abbf243
Matthew <i4matthew@i4tm.net>
2022-12-10 15:12:27 -0500

    Added Gun Container and started Gun Script

---
+++ Project Rocket/Assets/Scipts/PickUpController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    // Add Gun Script Here
+    [Header("References")]
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    [Header("HotKeys")]
+    public static KeyCode pickUpKey = KeyCode.E;
+    public static KeyCode dropKey = KeyCode.Q;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Check if player is in range and "PickUp Key" is Pressed
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(pickUpKey) && !slotFull) {PickUp();}
+
+        //Drop if equipped and "Drop Key" is pressed
+        if (equipped && Input.GetKeyDown(dropKey)) {Drop();}
+    }
+
+    private void PickUp(){
+        equipped = true;
+        slotFull = true;
+    }
+
+    private void Drop(){
+
+    }
+}
--- Project Rocket/Assets/Scipts/PlayerMovement.cs
+++ Project Rocket/Assets/Scipts/PlayerMovement.cs
@@ -33,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
     //public KeyCode sprintKey = KeyCode.LeftShift;
-    public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode crouchKey = KeyCode.C;
 
     [Header("Ground Check")]
     public float playerHeight;
