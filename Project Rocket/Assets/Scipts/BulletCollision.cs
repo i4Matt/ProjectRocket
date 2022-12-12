e0b17ac84c5d1af349d05a4e500dcf40e26ad561
Matthew <i4matthew@i4tm.net>
2022-12-11 19:51:11 -0500

    Cleanup Folders

--- Project Rocket/Assets/Scipts/BulletCollision.cs
+++ Project Rocket/Assets/Scipts/BulletCollision.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class BulletCollision : MonoBehaviour
 {
-
-
     [Header("Explosion")]
     public ParticleSystem exp;
 
