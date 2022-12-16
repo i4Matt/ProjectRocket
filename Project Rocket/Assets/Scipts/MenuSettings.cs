288dd8a5da91c02b23bc2b64b6f382fd85f6f124
D3RPSIDE <119375942+D3RPSIDEoftheMoon@users.noreply.github.com>
2022-12-16 14:35:18 -0800

    Volume Update

---
+++ Project Rocket/Assets/Scipts/MenuSettings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MenuSettings : MonoBehaviour
+{
+    public  AudioMixer audioMixer;
+   public void SetVolume (float volume)
+   {
+    audioMixer.SetFloat("Volume", volume);
+    Debug.Log(volume);
+   }
+
+}
