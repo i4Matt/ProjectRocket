7a6826092c856b63e8cfd15de2a40d6dedbfd87b
D3RPSIDE <nick.zenmods@gmail.com>
2022-12-16 17:26:12 -0800

    heath

--- Project Rocket/Assets/Scipts/Health.cs
+++ Project Rocket/Assets/Scipts/Health.cs
@@ -9,21 +9,34 @@ public class Health : MonoBehaviour
     public int maxHealth;
     public int currentHealth;
     public float respawnTime;
+    public GameObject heart1;
+    public GameObject heart2;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        if(gameObject.name == "Player")
+        {
+        heart1.SetActive(true);
+        heart2.SetActive(true);
+        }
     }
+
 
     public void TakeDamage()
     {
         currentHealth -= 1;
-
+        if(gameObject.name == "Player")
+        {
+        heart2.SetActive(false);
+        }
         if (currentHealth <= 0)
         {
+            
             if (gameObject.name == "Player")
             {
+                heart1.SetActive(false);
                 gameObject.GetComponent<PlayerDeath>().PlayerIsDead();
                 Invoke("ResetLevel", respawnTime / 1000f);
             }
